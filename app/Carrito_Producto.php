58dccb91598754e32ffde240cd46f340276b8ac4
deahtom123 <201504200@ingenieria.usac.edu.gt>
2018-09-17 12:57:16 -0600

    V0.0.2
    Creacion de Modelos

---
+++ app/Carrito_Producto.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Carrito_Producto extends Model
+{
+    protected $table = 'carrito_producto';
+}
---
+++ app/Factura_Producto.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Factura_Producto extends Model
+{
+    protected $table = 'factura_producto';
+}
