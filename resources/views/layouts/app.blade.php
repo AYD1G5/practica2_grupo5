9b118f628a729a2719166ffa21c61a4c68192027
deahtom123 <201504200@ingenieria.usac.edu.gt>
2018-09-17 11:23:52 -0600

    Estilo a la Plantila
    Se añadieron los archivos para la plantillas

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<!-- saved from url=(0030)https://bootswatch.com/flatly/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <title>Bases 1</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}" media="screen">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <!-- required modeler styles -->
+
+    <!-- needed for this example only -->
+    <script src="https://unpkg.com/jquery@3.3.1/dist/jquery.js"></script>
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+     <!-- example styles -->
+                @yield('encabezado')
+  </head>
+  <body style="" class="">
+  <div class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary" style="">
+      <div class="container">
+        <a href="#" class="navbar-brand">[AYD1] Practica 2</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation" style="">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            @guest
+            <ul class="nav navbar-nav ml-auto">
+                      
+                    </ul>
+
+        </div>
+      </div>
+    </div>
+<br />
+<br />
+<br />
+<br />
+    <div class="container">
+      <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+            @else
+            <ul class="navbar-nav">
+            </ul>
+            <ul class="nav navbar-nav ml-auto">
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
+
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+                  </ul>
+
+        </div>
+      </div>
+    </div>
+<br />
+<br />
+<br />
+<br />
+@if ($Bandera)
+  <div class="container">
+      <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+@else
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+              <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                   
+                    <H1>Cuenta Bloqueada</H1>
+                    No tienes permiso para logearte, ponte en comunicacion con un Administrador!
+               </div>
+               
+            </div>
+        </div>
+    </div>
+    
+</div>
+ 
+@endif
+@endguest
+
+   
+    <!-- Scripts -->
+      <script src="{{ asset('js/app.js') }}"></script>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/cargamasiva', function (){
+    return view('subir2');
+});
