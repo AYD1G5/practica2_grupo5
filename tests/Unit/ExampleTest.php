be50723fd41aa25602788b986ed6937914178185
WilliamLopez5 <willyslider@gmail.com>
2018-09-17 13:02:33 -0600

    V0.0.2
    Creacion de Preubas de Subtotal, total e IVA

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -4,8 +4,11 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Http\Funciones;
+
 
 class ExampleTest extends TestCase
 {
-   
+
+
 }
--- tests/Unit/eCommerceTest.php
+++ tests/Unit/eCommerceTest.php
@@ -8,5 +8,36 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class eCommerceTest extends TestCase
 {
+    /**
+    * Prueba para la funcion calcular SubTotal de Compra
+    */
+    public function testCalcularSubTotal()
+    {
+        $funciones=new Funciones(); 
+        
+        
+        $this->assertEquals(1500,$funciones->calcularSubTotal(5,300));
+    }
+
+   /**
+    * Prueba para la funcion calcular Total de Compra
+    */
+    public function testCalcularTotal()
+    {
+        $funciones=new Funciones(); 
+                
+        $this->assertEquals(1500,$funciones->calcularTotal());
+    }
+
+    /**
+    * Prueba para la funcion calcular IVA
+    */
+    public function testCalcularIva()
+    {
+        $funciones=new Funciones(); 
+                
+        $this->assertEquals(120,$funciones->calcularIva(1120));
     
+    }
+
 }
